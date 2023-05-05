5cf35ff646315c8a0937f4bfafee3b9aa7b69d49
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-05-05 20:34:19 +0300

    create files splash view and splash view body

---
+++ android/app/src/main/kotlin/com/example/easy_book/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.easy_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:easy_book/features/splash/presentation/view/widgets/splash_view.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
