2c2cdaaf41d223972bc37e587196f1c0ebe04490
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-06-16 20:55:44 +0300

    create newset books cubit state

---
+++ lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:easy_book/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../data/models/book_model/book_model.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit() : super(NewsetBooksInitial());
+
+  // final HomeRepo homeRepo;
+
+}
