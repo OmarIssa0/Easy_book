b73abc3b70b6f3aef0104814b7dd414a4c0f2508
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-06-16 20:57:51 +0300

    newset books cubit done

--- lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
@@ -7,8 +7,18 @@ import '../../../data/models/book_model/book_model.dart';
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
 
-  // final HomeRepo homeRepo;
+  final HomeRepo homeRepo;
 
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+
+    result.fold((failure) {
+      emit(NewsetBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewsetBooksSuccess(books));
+    });
+  }
 }
