a5d5f4a17d12d34528d454d5ba681a4e2f345a72
OmarIssa0 <omar.issa.slwade@gmail.com>
2023-06-16 18:37:05 +0300

    implement fetch newset books

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -4,7 +4,7 @@ import 'package:easy_book/features/home/data/models/book_model/book_model.dart';
 
 abstract class HomeRepo {
   //Recommanded for you book
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
 
   // popular books
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
